6f07e15fb7c41bc9e70e8b4dd4e4724071809a0f
Robert James Gabriel <robert_gabriel@outlook.com>
2024-05-08 23:45:02 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withMDX = require("@zeit/next-mdx")({
 });
 
 module.exports = withMDX({
+  exportPathMap: async function (defaultPathMap) {
+    return defaultPathMap;
+  },
   pageExtensions: ["js", "jsx", "mdx"]
   // assetPrefix: process.env.NODE_ENV === "production" ? "/lexend" : ""
 });
